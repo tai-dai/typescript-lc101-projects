1c52c32ee221266703a9ca950ed9186b814a81df
Tai Smith <imadeanomelet@gmail.com>
2020-10-27 09:23:15 -0500

    added classes and complied index: complete

---
+++ studio/Rocket.js
@@ -0,0 +1,46 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.totalCapacityKg = totalCapacityKg;
+        this.name = name;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var sumMass = 0;
+        for (var i = 0; i < items.length; i++) {
+            sumMass += items[i].massKg;
+        }
+        return sumMass;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return (this.sumMass(this.astronauts) + this.sumMass(this.cargoItems));
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut) === true) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
